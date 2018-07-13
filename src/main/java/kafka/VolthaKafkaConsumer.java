42f6cdd0cb308d4efc128682b23215176f6638c5
William Kurkian <wkurkian@cisco.com>
2018-07-13 17:05:28 -0400

    Updated error catching for different failure scenarios

--- src/main/java/kafka/VolthaKafkaConsumer.java
+++ src/main/java/kafka/VolthaKafkaConsumer.java
@@ -3,7 +3,7 @@ import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
-
+import org.apache.kafka.common.KafkaException;
 import java.util.Collections;
 import java.util.Properties;
 import org.slf4j.Logger;
@@ -11,14 +11,14 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.Marker;
 import org.slf4j.MarkerFactory;
 
+import javax.xml.ws.http.HTTPException;
+
+import java.util.concurrent.TimeUnit;
+
 import ves.*;
 import config.Config;
 
 public class VolthaKafkaConsumer {
-
-//    private final static String TOPIC = "voltha.alarms";
-//    private final static String BOOTSTRAP_SERVERS =
-//            "kafka.voltha.svc.cluster.local:9092";
 
     private final Logger logger = LoggerFactory.getLogger("VolthaKafkaConsumer");
     private final String dataMarkerText = "DATA";
@@ -39,7 +39,6 @@ public class VolthaKafkaConsumer {
     private KafkaConsumer<Long, String> createConsumer() {
       logger.debug("Creating Kafka Consumer");
 
-
       String kafkaAddress = Config.getKafkaAddress() + ":" + Config.getKafkaPort();
       final Properties props = new Properties();
       props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
@@ -50,6 +49,8 @@ public class VolthaKafkaConsumer {
               LongDeserializer.class.getName());
       props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
               StringDeserializer.class.getName());
+      props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,
+              false);
 
       // Create the consumer using props.
       final KafkaConsumer<Long, String> consumer =
@@ -62,38 +63,44 @@ public class VolthaKafkaConsumer {
 
   public void runConsumer() throws InterruptedException {
 
-	logger.debug("Starting Consumer");
+      logger.debug("Starting Consumer");
 
-        while (true) {
-            final ConsumerRecords<Long, String> consumerRecords =
-                    consumer.poll(100000);
+      while (true) {
+	  ConsumerRecords<Long, String> consumerRecords;
+ 	  try {
+	  	consumerRecords = consumer.poll(100000);
+	  } catch (KafkaException e) {
+		logger.debug("Error with Kafka connection. Retrying in 15 seconds.");
+		this.consumer = createConsumer();
+		TimeUnit.SECONDS.sleep(15);
+		continue;
+	  }
+          logger.info("{} Records retrieved from poll.", consumerRecords.count());
 
+          boolean commit = true;
+          try {
             consumerRecords.forEach(record -> {
-                logger.info(dataMarker, "Consumer Record:({}, {}, {}, {})\n",
-                        record.key(), record.value(),
-                        record.partition(), record.offset());
-                logger.info("Attempting to send data to VES");
-                boolean success = false;
-                while (!success) {
-                  success = VesAgent.sendToVES(record.value());
-                  if (!success) {
-                    logger.info("Ves message failed. Sleeping for 15 seconds.");
-                    try {
-                      Thread.sleep(15000);
-                    } catch (InterruptedException e) {
-                      logger.info(e.getMessage());
-                    }
-                  } else {
-                    logger.info("Sent Ves Message");
-                  }
-                }
-
+              logger.info(dataMarker, "Consumer Record:({}, {}, {}, {})\n",
+                      record.key(), record.value(),
+                      record.partition(), record.offset());
+              logger.info("Attempting to send data to VES");
+              boolean success = VesAgent.sendToVES(record.value());
+              if (!success) {
+                throw new HTTPException(0);
+              } else {
+                logger.info("Sent Ves Message");
+              }
             });
-
+          } catch (HTTPException e) {
+            logger.info("Ves message failed. Going back to polling.");
+            commit = false;
+          }
+          if (commit) {
             consumer.commitAsync();
-        }
-        //consumer.close();
-        //logger.debug("DONE");
+          }
+      }
+      //consumer.close();
+      //logger.debug("DONE");
     }
 
 }
